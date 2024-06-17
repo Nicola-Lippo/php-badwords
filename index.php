bfb5b13c714410267084410a72d5b0efc262644a
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-17 16:12:36 +0200

    First commit, prova php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//UN SOLO ELEMENTO PUO' STARE ANCHE SENZA ;
+$key = 'Mondo';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <div>Ciaooo <?php echo $key; ?></div>
+</body>
+</html>
