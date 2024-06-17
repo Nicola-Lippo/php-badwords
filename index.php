dc40b2c16054d71365825b3b2fe9097880c93a31
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-17 18:07:27 +0200

    [UPD] struttura form + collegamento form

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<?php
-//UN SOLO ELEMENTO PUO' STARE ANCHE SENZA ;
-$key = 'Mondo';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
 </head>
+
 <body>
-    <div>Ciaooo <?php echo $key; ?></div>
+    <!--action indico dove inviare i dati method se get o post-->
+    <form action="risposta.php" method="GET">
+        <div>
+            <!--CAMPO DI TESTO-->
+            <div>
+                <label for="text">Inserisci un testo</label>
+                <input type="text" id="text">
+            </div>
+            <!--PAROLA DA RIMUOVERE-->
+            <div>
+                <label for="">Parola da rimuovere</label>
+                <input type="text">
+            </div>
+            <!--BOTTONE INVIO-->
+            <div>
+                <button>Invia i tuoi dati al server</button>
+            </div>
+        </div>
+    </form>
 </body>
+
 </html>
